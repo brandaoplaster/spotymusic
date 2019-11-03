1de960b656778f4243c9ecbc0e676eb9b5ca40bf
brandaoplaster <brandaoplaster@gmail.com>
2019-11-03 15:45:15 -0300

    Changed routes including app routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,26 @@
 Rails.application.routes.draw do
-  get 'favorites/index'
-  get 'favorites/create'
-  get 'favorites/destroy'
-  get 'albums/show'
-  get 'artists/show'
-  get 'categories/show'
-  get 'search/index'
-  get 'search/new'
-  get 'dashboard/index'
-  get 'home/index'
+  get 'recently_heards/create'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+    resources :favorites, only: :index
+    root to: "dashboard#index", as: :authenticated_root
+    resources :search, only: [:index, :new], as: :searches
+    resources :categories, only: :show
+    resources :artists, only: :show
+
+    resources :albums, only: :show do
+      resources :recently_heards, only: :create
+    end
+
+    resources :songs, only: [] do
+      post "/favorite", to: "favorites#create", on: :member, defaults: { format: :js, favoritable_type: 'Song' }
+      delete "/favorite", to: "favorites#destroy", on: :member, defaults: { format: :js, favoritable_type: 'Song' }
+    end
+  end
+
+  unauthenticated :user do
+    root to: "home#index"
+  end
+
 end
